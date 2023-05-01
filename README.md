61379e21e6640f5b30b7dae9dd4a28df3d04a24f
LightEx <dimon.melnik.c@gmail.com>
2023-05-01 17:16:59 +0300

    Edited README.md and rewrote image_classificator.py to OOP

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Testing ReadMe file usage
+This programm should run by without any editing as long as all libraries are installed. Pictures that are neccessary for this wil be downloaded automatically in "C:\Users\USER_NAME\.keras\datasets" folder
+
+Most of us libraries used in this project are preinstalled with python. Those that not preinstalled will be listed below.
+-Tensorflow version: 2.12.0
+
+For testing purposes you can either create and train model by yourself (which takes 12 minutes on my pc) or load already trained model.
+To choose one of them comment/uncomment last lines of code
--- image_classificator.py
+++ image_classificator.py
@@ -1,121 +1,178 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-
 from tensorflow.python.keras import layers
 from tensorflow.python.keras.models import Sequential
-
 import pathlib
-
-dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-data_dir = tf.keras.utils.get_file("flower_photos", origin=dataset_url, untar=True)
-data_dir = pathlib.Path(data_dir)
-
-
-batch_size = 32
-img_height = 180
-img_width = 180
-
-
-train_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="training",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size,
-)
-
-val_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="validation",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size,
-)
-
-class_names = train_ds.class_names
-
 import matplotlib.pyplot as plt
 
-plt.figure(figsize=(10, 10))
-for images, labels in train_ds.take(1):
-    for i in range(9):
-        ax = plt.subplot(3, 3, i + 1)
-        plt.imshow(images[i].numpy().astype("uint8"))
-        plt.title(class_names[labels[i]])
-        plt.axis("off")
 
-AUTOTUNE = tf.data.AUTOTUNE
+class ImageClassificator:
+    def __init__(self, img_height, img_width, batch_size):
+        self.img_height = img_height
+        self.img_width = img_width
+        self.batch_size = batch_size
 
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+    def load_dataset(self, dataset_url=None, dataset_name=None, validation_spit=0.2):
+        data_dir = tf.keras.utils.get_file(dataset_name, origin=dataset_url, untar=True)
+        self.data_dir = pathlib.Path(data_dir)
 
-normalization_layer = tf.keras.layers.Rescaling(scale=1.0 / 255, offset=0.0)
+        self.train_ds = tf.keras.utils.image_dataset_from_directory(
+            self.data_dir,
+            validation_split=validation_spit,
+            subset="training",
+            seed=123,
+            image_size=(self.img_height, self.img_width),
+            batch_size=self.batch_size,
+        )
+
+        self.val_ds = tf.keras.utils.image_dataset_from_directory(
+            self.data_dir,
+            validation_split=validation_spit,
+            subset="validation",
+            seed=123,
+            image_size=(self.img_height, self.img_width),
+            batch_size=self.batch_size,
+        )
+        self.class_names = self.train_ds.class_names
+
+    def show_accuracy_graph(self):
+        acc = self.history.history["accuracy"]
+
+        val_acc = self.history.history["val_accuracy"]
+
+        loss = self.history.history["loss"]
+        val_loss = self.history.history["val_loss"]
+
+        epochs_range = range(self.epochs)
+
+        plt.figure(figsize=(8, 8))
+        plt.subplot(1, 2, 1)
+        plt.plot(epochs_range, acc, label="Training Accuracy")
+        plt.plot(epochs_range, val_acc, label="Validation Accuracy")
+        plt.legend(loc="lower right")
+        plt.title("Training and Validation Accuracy")
+
+        plt.subplot(1, 2, 2)
+        plt.plot(epochs_range, loss, label="Training Loss")
+        plt.plot(epochs_range, val_loss, label="Validation Loss")
+        plt.legend(loc="upper right")
+        plt.title("Training and Validation Loss")
+        plt.show()
+
+    def train_model(self, epochs, optimizer="adam"):
+        self.epochs = epochs
+        self.model.compile(
+            optimizer=optimizer,
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=["accuracy"],
+        )
+        self.history = self.model.fit(
+            self.train_ds, validation_data=self.val_ds, epochs=epochs
+        )
+
+    def save_model(self, path):
+        self.model.save(path)
+
+    def cache_dataset(self):
+        AUTOTUNE = tf.data.AUTOTUNE
+        self.train_ds = (
+            self.train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+        )
+        self.val_ds = self.val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+    def add_data_augmentation(self, random_rotation=0.1, random_zoom=0.1):
+        self.data_augmentation = tf.keras.Sequential(
+            [
+                tf.keras.layers.RandomFlip(
+                    "horizontal", input_shape=(self.img_height, self.img_width, 3)
+                ),
+                tf.keras.layers.RandomRotation(random_rotation),
+                tf.keras.layers.RandomZoom(random_zoom),
+            ]
+        )
+
+    def create_model(self, dropout_percent=0.2):
+        num_classes = len(self.class_names)
+        self.model = Sequential(
+            [
+                self.data_augmentation,
+                tf.keras.layers.Rescaling(1.0 / 255),
+                layers.Conv2D(16, 3, padding="same", activation="relu"),
+                layers.MaxPooling2D(),
+                layers.Conv2D(32, 3, padding="same", activation="relu"),
+                layers.MaxPooling2D(),
+                layers.Conv2D(64, 3, padding="same", activation="relu"),
+                layers.MaxPooling2D(),
+                layers.Dropout(dropout_percent),
+                layers.Flatten(),
+                layers.Dense(128, activation="relu"),
+                layers.Dense(num_classes, name="outputs"),
+            ]
+        )
+
+    def load_model(self, path):
+        self.model = tf.keras.models.load_model(path)
+
+    def predict(self, img_array):
+        predictions = self.model.predict(img_array)
+        score = tf.nn.softmax(predictions[0])
+
+        print(
+            "This image most likely belongs to {} with a {:.2f} percent confidence.".format(
+                self.class_names[np.argmax(score)], 100 * np.max(score)
+            )
+        )
 
 
-num_classes = len(class_names)
+def create_model():
+    img_height = 180
+    img_width = 180
+    batch_size = 32
 
-data_augmentation = tf.keras.Sequential(
-    [
-        tf.keras.layers.RandomFlip(
-            "horizontal", input_shape=(img_height, img_width, 3)
-        ),
-        tf.keras.layers.RandomRotation(0.1),
-        tf.keras.layers.RandomZoom(0.1),
-    ]
-)
+    model = ImageClassificator(img_height, img_width, batch_size)
+    model.load_dataset(
+        "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz",
+        "flower_photos",
+    )
 
+    model.cache_dataset()
+    model.add_data_augmentation()
+    model.create_model()
+    model.train_model(20)
+    model.save_model("saved_model/model2")
 
-model = Sequential(
-    [
-        data_augmentation,
-        tf.keras.layers.Rescaling(1.0 / 255),
-        layers.Conv2D(16, 3, padding="same", activation="relu"),
-        layers.MaxPooling2D(),
-        layers.Conv2D(32, 3, padding="same", activation="relu"),
-        layers.MaxPooling2D(),
-        layers.Conv2D(64, 3, padding="same", activation="relu"),
-        layers.MaxPooling2D(),
-        layers.Dropout(0.2),
-        layers.Flatten(),
-        layers.Dense(128, activation="relu"),
-        layers.Dense(num_classes, name="outputs"),
-    ]
-)
+    sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
+    sunflower_path = tf.keras.utils.get_file("Red_sunflower", origin=sunflower_url)
+    img = tf.keras.utils.load_img(sunflower_path, target_size=(img_height, img_width))
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
 
-model.compile(
-    optimizer="adam",
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=["accuracy"],
-)
-
-epochs = 20
-history = model.fit(train_ds, validation_data=val_ds, epochs=epochs)
+    model.predict(img_array)
+    model.show_accuracy_graph()
 
 
-model.save("saved_model/my_model")
+def load_model():
+    img_height = 180
+    img_width = 180
+    batch_size = 32
+    model = ImageClassificator(img_height, img_width, batch_size)
+    model.load_model("saved_model/my_model")
+    model.load_dataset(
+        "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz",
+        "flower_photos",
+    )
+
+    sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
+    sunflower_path = tf.keras.utils.get_file("Red_sunflower", origin=sunflower_url)
+
+    img = tf.keras.utils.load_img(sunflower_path, target_size=(img_height, img_width))
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
+
+    model.predict(img_array)
 
 
-acc = history.history["accuracy"]
-val_acc = history.history["val_accuracy"]
-
-loss = history.history["loss"]
-val_loss = history.history["val_loss"]
-
-epochs_range = range(epochs)
-
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label="Training Accuracy")
-plt.plot(epochs_range, val_acc, label="Validation Accuracy")
-plt.legend(loc="lower right")
-plt.title("Training and Validation Accuracy")
-
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label="Training Loss")
-plt.plot(epochs_range, val_loss, label="Validation Loss")
-plt.legend(loc="upper right")
-plt.title("Training and Validation Loss")
-plt.show()
+if __name__ == "__main__":
+    # create_model()
+    load_model()
